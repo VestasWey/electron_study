6ce5bb5a51607d6570f4737a05464d5c3868d1e9
Vestas.Wey <Vestas.Wey@qq.com>
2020-04-28 18:50:39 +0800

    搭建一个gn项目

---
+++ natives/modules/ffapi-study/ffapi_study.cpp
@@ -0,0 +1,87 @@
+﻿// elecstudy.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "ffapi_study.h"
+
+namespace
+{
+    /*fnStrCallback1 str_cb1 = nullptr;
+    fnStrCallback2 str_cb2 = nullptr;
+    fnStrCallback3 str_cb3 = nullptr;
+
+    fnpCharCallback1 pchar_cb1 = nullptr;
+    fnpCharCallback2 pchar_cb2 = nullptr;
+    fnpCharCallback3 pchar_cb3 = nullptr;
+
+    fnppCharCallback1 ppchar_cb1 = nullptr;
+    fnppCharCallback2 ppchar_cb2 = nullptr;
+    fnppCharCallback3 ppchar_cb3 = nullptr;*/
+}
+
+// 这是导出变量的一个示例
+ELECSTUDY_API int nelecstudy=0;
+
+// 这是导出函数的一个示例。
+ELECSTUDY_API int fn_add(int a, int b)
+{
+    return a + b;
+}
+
+
+ELECSTUDY_API void fn_set_str_callback1(fnStrCallback1 callback)
+{
+    callback("const std::string& str");
+}
+
+ELECSTUDY_API void fn_set_str_callback2(fnStrCallback2 callback)
+{
+    std::string str("std::string& str");
+    callback(str);
+}
+
+ELECSTUDY_API void fn_set_str_callback3(fnStrCallback3 callback)
+{
+    callback("std::string& str");
+}
+
+
+ELECSTUDY_API void fn_set_pchar_callback1(fnpCharCallback1 callback)
+{
+    callback("const char* str");
+}
+
+ELECSTUDY_API void fn_set_pchar_callback2(fnpCharCallback2 callback)
+{
+    callback("const char* const str");
+}
+
+ELECSTUDY_API void fn_set_pchar_callback3(fnpCharCallback3 callback)
+{
+    callback(const_cast<char*>("char*"));
+}
+
+
+ELECSTUDY_API void fn_set_ppchar_callback1(fnppCharCallback1 callback)
+{
+    const char* pstr = "const char** str";
+    callback(&pstr);
+}
+
+ELECSTUDY_API void fn_set_ppchar_callback2(fnppCharCallback2 callback)
+{
+    const char* pstr = "const char** const str";
+    callback(&pstr);
+}
+
+ELECSTUDY_API void fn_set_ppchar_callback3(fnppCharCallback3 callback)
+{
+    const char* pstr = "char** str";
+    callback(const_cast<char**>(&pstr));
+}
+
+// 这是已导出类的构造函数。
+Celecstudy::Celecstudy()
+{
+    return;
+}
---
+++ natives/modules/ffapi-study/ffapi_study.h
@@ -0,0 +1,56 @@
+﻿#include <string>
+// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 ELECSTUDY_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// ELECSTUDY_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef ELECSTUDY_EXPORTS
+#define ELECSTUDY_API __declspec(dllexport)
+#else
+#define ELECSTUDY_API __declspec(dllimport)
+#endif
+
+// 此类是从 dll 导出的
+class ELECSTUDY_API Celecstudy {
+public:
+	Celecstudy(void);
+	// TODO: 在此处添加方法。
+};
+
+extern ELECSTUDY_API int nelecstudy;
+
+extern "C"
+{
+    ELECSTUDY_API int fn_add(int a, int b);
+
+
+    typedef void(*fnStrCallback1) (const std::string& str);
+    ELECSTUDY_API void fn_set_str_callback1(fnStrCallback1 callback);
+
+    typedef void(*fnStrCallback2) (std::string& str);
+    ELECSTUDY_API void fn_set_str_callback2(fnStrCallback2 callback);
+
+    typedef void(*fnStrCallback3) (std::string str);
+    ELECSTUDY_API void fn_set_str_callback3(fnStrCallback3 callback);
+
+
+    typedef void(*fnpCharCallback1) (const char* str);
+    ELECSTUDY_API void fn_set_pchar_callback1(fnpCharCallback1 callback);
+
+    typedef void(*fnpCharCallback2) (const char* const str);
+    ELECSTUDY_API void fn_set_pchar_callback2(fnpCharCallback2 callback);
+
+    typedef void(*fnpCharCallback3) (char* str);
+    ELECSTUDY_API void fn_set_pchar_callback3(fnpCharCallback3 callback);
+
+
+    typedef void(*fnppCharCallback1) (const char** str);
+    ELECSTUDY_API void fn_set_ppchar_callback1(fnppCharCallback1 callback);
+
+    typedef void(*fnppCharCallback2) (const char** const str);
+    ELECSTUDY_API void fn_set_ppchar_callback2(fnppCharCallback2 callback);
+
+    typedef void(*fnppCharCallback3) (char** str);
+    ELECSTUDY_API void fn_set_ppchar_callback3(fnppCharCallback3 callback);
+}
